49a95867c3d6f0ad0c81244eff8fe7e15859526b
teo22w-max <teo.22w@gmail.com>
2026-03-17 15:47:38 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+# print('salom, Python!')
+
+# ism = 'ali'
+# yosh = 15
+# print(ism)
+# print(yosh)
+
+# ism = 'Ali'
+# yosh = 15
+# print(ism, yosh, 'yoshda')
+
+# a = 10
+# b = 3
+# print(a + b)
+# print(a * b)
+
+# ism = input('Ismingni yoz: ')
+# print(ism)
+
+# yosh = int(input('Yoshingni yoz: '))
+# print(yosh + 1)
+
+# ism = input('Ism: ')
+# yosh = int(input('Yosh: '))
+# print(f'{ism} {yosh} yoshda')
+
+# ism = 'Ali'
+# yosh = 15
+# print(ism, yosh, 'yoshda')
+
+# yosh = 15
+
+# if yosh >= 18:
+#     print('siz katta odamsiz')
+# else:
+#     print('siz hali kichiksiz')
+
+# son = int(input('son yoz: '))
+
+# if 15 == 10:
+#     print('Togri topding!')
+# else:
+#     print('Boshqa son')
+
+
+# son = int(input('son yoz: '))
+
+# if son % 2 == 0:
+#     print('Juft son')
+# else:
+#     print('Toq son')
+
+
+# son = int(input('son yoz: '))
+
+# if son > 0:
+#     print('Musbat son')
+# elif son < 0:
+#     print('Manfiy son')
+# else:
+#     print('Nol')
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1, 6):
+#     print(i)
+
+# a= 'futbol'
+# b = 'zor'
+# print(b, a)
+
+for i in range(1,11):
+    print(i)
