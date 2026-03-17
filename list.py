6789d670d813485871742a8c14b43930dd324351
teo22w-max <teo.22w@gmail.com>
2026-03-17 15:42:32 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,57 @@
+for i in range(1, 10):
+    for j in range(1, 11):
+        print(f"{i} * {j} = {i * j}")
+    print("-" * 10)  
+
+# for i in range(1,10):
+#     print(f'{i} ning karra jadvali')
+#     for j in range(1,11):
+#         print(f"{i} * {j} = {i * j}")
+ 
+# def qoshish(num1,num2):
+#     return num1 + num2
+
+# print(qoshish(3,5))
+
+# def puzzle1 (items):
+#     summ = 1
+#     for items in items:
+#         summ = summ * items
+#         return summ
+
+
+# list1 = [1,2,3,4,5]
+# print(puzzle1(list1))
+
+# list1 = [3,23,4,76,100]
+# def puzzle2 (items) :
+#     result = 1
+#     for items in items:
+#         result = result * items
+
+#     return result
+
+# print(puzzle2(list1))
+
+
+# def puzzle3 (items) :
+#     result = 0
+#     for item in items:
+#         if item > result :
+#             result = item
+#     return result
+
+# print(puzzle3(list1))
+# list1 = []
+
+# if len(list1) == 0:
+#     print(True) 
+
+# def puzzle1 (items) :
+#     result = 0
+#     for item in items :
+#         result = item
+#     return result
+
+# print(puzzle1(list1) == 0)
+
